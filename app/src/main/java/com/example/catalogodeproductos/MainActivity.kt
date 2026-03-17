37038b434660d6b129507b9d0e83c887ede7daff
Juan Andres Leandro Rodriguez <88171229+Juan2307@users.noreply.github.com>
2026-03-16 23:58:31 -0500

    🚀 feat: Base Del Catálogo

--- app/src/main/java/com/example/catalogodeproductos/MainActivity.kt
+++ app/src/main/java/com/example/catalogodeproductos/MainActivity.kt
@@ -1,25 +1,42 @@
 package com.example.catalogodeproductos
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.commit
 
-class MainActivity : AppCompatActivity() {
+// La MainActivity implementa la interfaz del MenuFragment para poder recibir los eventos de clic.
+class MainActivity : AppCompatActivity(), MenuFragment.OnOptionClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
-
-        enableEdgeToEdge()
-
         setContentView(R.layout.activity_main)
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        // Si es la primera vez que se crea la actividad, carga el fragmento por defecto.
+        // Esto evita que se recargue el fragmento al girar la pantalla, por ejemplo.
+        if (savedInstanceState == null)
+            onOptionClicked("profile")
+
+    }
+
+    // Este es el método obligatorio que implementa la interfaz. Se ejecuta cuando se pulsa un botón en el menú.
+    override fun onOptionClicked(option: String) {
+
+        // Usa una expresión 'when' (similar a un switch) para decidir qué fragmento mostrar.
+        val fragment: Fragment = when (option) {
+            "photos" -> PhotosFragment()
+            "video" -> VideoFragment()
+            "web" -> WebFragment()
+            else -> ProfileFragment() // Caso por defecto
+        }
+
+        // Inicia una transacción de fragmentos para reemplazar el contenido del contenedor derecho.
+        supportFragmentManager.commit {
+
+            replace(R.id.content_fragment_container, fragment)
+            setReorderingAllowed(true) // Optimización para las transiciones
+
         }
 
     }
